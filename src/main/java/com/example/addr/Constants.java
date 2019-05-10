99efd9c9e264cbb6481d914356a3104e070c494f
HabibaElwany <36865836+HabibaElwany@users.noreply.github.com>
2019-05-11 01:36:26 +0200

    addr

---
+++ src/main/java/com/example/addr/Constants.java
@@ -0,0 +1,10 @@
+package com.example.addr;
+
+public class Constants {
+    // private static final String ROOT_URL ="http://192.168.1.1/graduation/includes/";
+    private static String ROOT_URL = "http://192.168.1.106/graduation/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+//    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
